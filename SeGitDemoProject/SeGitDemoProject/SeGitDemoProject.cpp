6ef4ff52a387bb0faeb7ede027ffa9309d58556a
DonatasKozlovskis <donatas.kozlovskis@gmail.com>
2013-09-26 00:17:03 +0200

    Alligned code lines

--- SeGitDemoProject/SeGitDemoProject/SeGitDemoProject.cpp
+++ SeGitDemoProject/SeGitDemoProject/SeGitDemoProject.cpp
@@ -3,20 +3,18 @@
 
 using namespace std;
 
-
-
 int main(){
-	int i = 1, j ;
+	int i = 1;
+	int j;
 	system("COLOR 2");
 	while (i<100000){
 		j = rand() % 10;
 		cout << j << " ";
 
 		i+=1;
-
 	}
+
 	cout << "\nMuhahaha!!! Matrix?? :D \n";
 	system("pause");
-
 	return 0;
 }
